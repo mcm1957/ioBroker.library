25fdcb5530158bad9438c95aae77a538fead7d3f
mcm1957 <mcm57@gmx.at>
2023-03-13 19:30:40 +0100

    first release

---
+++ library.js
@@ -0,0 +1,15 @@
+/**
+ *  library.js - iob support library
+ *
+ *		copyright McM1957 2023, MIT
+ *
+ */
+// @ts-ignore
+
+const iobTranslator = require('./lib/translib.js');
+
+module.exports =
+    {
+        iobTranslator
+    };
+
